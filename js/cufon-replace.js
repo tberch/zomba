1f14202063e6dcfc414e9f3cbd3a33afbbe4223c
Tom Berchenbriter <tom@zuit.net>
2015-02-07 19:16:56 -0800

    first commit

---
+++ js/cufon-replace.js
@@ -0,0 +1,3 @@
+Cufon.replace('h2', { fontFamily: 'Myriad Pro Cond' });
+Cufon.replace('h3, h4', { fontFamily: 'Myriad Pro' });
+Cufon.replace('#header .nav a', { fontFamily: 'Myriad Pro Cond', hover:true });
